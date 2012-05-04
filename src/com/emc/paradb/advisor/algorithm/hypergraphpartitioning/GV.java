0960b5ce64119fb1f82e351e52d07068b20aa4d5
Xiaoyan Guo <tuxgxy@gmail.com>
2012-05-04 14:15:55 +0800

    add hypergraph partitioning scheme using hypergraph

---
+++ src/com/emc/paradb/advisor/algorithm/hypergraphpartitioning/GV.java
@@ -0,0 +1,11 @@
+package com.emc.paradb.advisor.algorithm.hypergraphpartitioning;
+
+
+
+public class GV
+{
+	static final String dirName = "test";
+	static final int nWay = 100;
+	
+	static final String graphFileName = "graphFile";
+}
---
+++ src/com/emc/paradb/advisor/algorithm/hypergraphpartitioning/Predicate.java
@@ -0,0 +1,83 @@
+package com.emc.paradb.advisor.algorithm.hypergraphpartitioning;
+
+
+public class Predicate
+{
+	private Integer min = null;
+	private Integer max = null;
+	private Integer size = null;
+	
+	public Predicate()
+	{}
+	
+	public Predicate(int min, int max)
+	{
+		this.min = min;
+		this.max = max;
+		//size = 1;
+	}
+	
+	public Predicate(Predicate copyP)
+	{
+		min = copyP.min;
+		max = copyP.max;
+		size = copyP.size;
+	}
+	public Integer getSize()
+	{
+		if(size == null)
+			System.err.println("Err: predicate has no size");
+		return size;
+	}
+	
+	public void setSize(int size)
+	{
+		this.size = size;
+	}
+	
+	
+	public Integer getMin()
+	{
+		return min;
+	}
+	public Integer getMax()
+	{
+		return max;
+	}
+	
+	public void setMin(int min)
+	{
+		this.min = min;
+	}
+	public void setMax(int max)
+	{
+		this.max = max;
+	}
+	
+	public boolean match(Predicate searchP)
+	{
+		int sMin = searchP.getMin();
+		int sMax = searchP.getMax();
+		
+		//Note that, we add this condition manually
+		if(sMin >= sMax)
+			return true;
+		
+		if(sMin >= min && sMin < max ||
+				sMax > min && sMax < max ||
+					sMin < min && sMax >= max)
+			return true;
+		else
+			return false;
+	}
+	
+	public int hashCode()
+	{
+		return (min + max) / 2;
+	}
+	
+	public String toString()
+	{
+		return min +":"+ max + ":" + size;
+	}
+}
